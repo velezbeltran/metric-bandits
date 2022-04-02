efc20dd1f2cdfc91307a58c3f505650c3ecaf6dd
Nicolas Beltran <nb2838@columbia.edu>
2022-04-02 11:21:02 -0400

    fixed venv issue

--- metric_bandits/data/mnist.py
+++ metric_bandits/data/mnist.py
@@ -2,8 +2,14 @@
 A simple class to keep thing uniformly formatted.
 """
 
-from torchvision import datasets
+from torchvision import datasets, transforms
+
 
 from metric_bandits.constants.paths import MNIST_PATH
 
-MNIST = datasets.MNIST(MNIST_PATH, train=False, download=True)
+MNIST = datasets.MNIST(
+    MNIST_PATH,
+    train=False,
+    download=True,
+    transform=transforms.ToTensor()
+)
