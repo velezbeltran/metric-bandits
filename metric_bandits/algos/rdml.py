803454876b7ad9d11a6289205310011a4e08e9e8
Ketan Jog <kj2473@columbia.edu>
2022-04-07 01:51:54 -0400

    wine data script.

---
+++ metric_bandits/algos/rdml.py
@@ -0,0 +1,6 @@
+"""
+The implementation of  RDML - regularised distance metric learning, 
+follows the online-reg algorithm described in 
+the paper:
+`https://papers.nips.cc/paper/2009/file/a666587afda6e89aec274a3657558a27-Paper.pdf`
+"""
