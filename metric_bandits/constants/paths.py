9effb2f7fa598a07314f27f7ca07fed3cb2ef72d
Nicolas Beltran <nb2838@columbia.edu>
2022-04-13 17:21:01 -0400

    added etc folder to paths

--- metric_bandits/constants/paths.py
+++ metric_bandits/constants/paths.py
@@ -8,11 +8,16 @@ from pathlib import Path
 # Base paths for the project.
 ROOT_PATH = Path(__file__).parent.parent.parent
 DATA_PATH = os.path.join(ROOT_PATH, "data")
+ETC_PATH = os.path.join(ROOT_PATH, "etc")
+
+# object dump path
+OBJECT_DUMP_PATH = os.path.join(ETC_PATH, "object_dump")
 
 # Paths for report
 WRITEUP_PATH = os.path.join(ROOT_PATH, "writeup")
 REPORT_PATH = os.path.join(WRITEUP_PATH, "report")
 FIGURES_PATH = os.path.join(REPORT_PATH, "figures")
+
 
 # CIFAR_10 paths
 CIFAR_10_PATH = os.path.join(DATA_PATH, "cifar-10-batches-py")
