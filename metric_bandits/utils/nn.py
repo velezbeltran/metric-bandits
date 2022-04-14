6f4736bf48817a11271bdfac62234b025caea345
Nicolas Beltran <nb2838@columbia.edu>
2022-04-14 12:12:02 -0400

    added function that takes models and makes them metrics

--- metric_bandits/utils/nn.py
+++ metric_bandits/utils/nn.py
@@ -1,6 +1,7 @@
 """
 Utilities related to neural networks.
 """
+import torch
 
 
 def make_batch(x):
@@ -10,3 +11,28 @@ def make_batch(x):
     if len(x.shape) == 1:
         x = x.unsqueeze(0)
     return x
+
+
+def to_tensor(x):
+    """
+    Converts x into a torch.Tensor if it isn't already
+    """
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x)
+    return x
+
+
+def make_metric(model):
+    """
+    Takes a model and using its embedding function returns a metric.
+    Assumes that x, y are numpy arrays.
+    """
+
+    def metric(x, y):
+        x = make_batch(to_tensor(x))
+        y = make_batch(to_tensor(y))
+
+        sim, _ = model.predict_similarity(x, y)
+        return sim.item()
+
+    return metric
