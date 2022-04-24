c18663adf4860347d6cb636a551502a88eaf1963
Ketan Jog <kj2473@columbia.edu>
2022-04-23 20:37:13 -0400

    active learning for linucb

--- metric_bandits/algos/linucb.py
+++ metric_bandits/algos/linucb.py
@@ -3,6 +3,8 @@ This is an implementation of LinUCB: UCB with linear hypothesis
 """
 
 from math import sqrt
+import random
+from collections import defaultdict
 from metric_bandits.algos.base import BaseAlgo
 import numpy as np
 from metric_bandits.utils.math import square_matrix_norm, sherman_morrison
@@ -15,11 +17,12 @@ class LinUCB(BaseAlgo):
     def __init__(
         self,
         matrix_tuning_parameter=1,
-        ucb_parameter=1
+        explore_param=1,
+        active=False
     ):
         # Tuning parameter, set  to 1 by default.
         self._lambda = matrix_tuning_parameter
-        self.beta = ucb_parameter
+        self.explore_param = explore_param
 
         # Starting state:
         self.A = None
@@ -28,13 +31,24 @@ class LinUCB(BaseAlgo):
         self.t = 0
         self.theta = None
         self.dim = 0
+        self.active=active
 
          # parameters to keep track of
         self.last_action = None
         self.rewards = []
         self.contexts_played = []
 
+
     def choose_action(self, actions):
+        """
+        actions is a list-like object dictionary and contains the available actions
+        """
+        if self.active:
+            return self.choose_action_active(actions)
+        else:
+            return self.choose_action_default(actions)
+
+    def choose_action_default(self, actions):
         """
         actions is a list-like object dictionary and contains the available actions
         """
@@ -48,7 +62,7 @@ class LinUCB(BaseAlgo):
             self.A_inv = self.A
             self.b = torch.zeros(self.dim)
 
-        
+    
         self.theta = torch.matmul(self.A_inv, self.b)
 
         # Loop through all available action, context pairs
@@ -60,11 +74,57 @@ class LinUCB(BaseAlgo):
         
         return self.last_action
 
+    def choose_action_active(self, actions):
+
+        # At step 0, initialise all variables
+        if self.t == 0:
+            # Get the dimension of (x,x',a) vector
+            self.dim = len(list(actions.values())[0])
+            self.A = torch.eye(self.dim)
+            self.A_inv = self.A
+            self.b = torch.zeros(self.dim)
+
+        self.theta = torch.matmul(self.A_inv, self.b)
+
+        self.mean_estimate = defaultdict(list)
+        self.ucb_estimate = defaultdict(int)
+        self.unique_contexts = defaultdict(list)
+
+        # Keep track of relevant values per unique context
+
+        # We shuffle in case we have an optimist reward of zero
+        items = list(actions.items())
+        random.shuffle(items)
+        for action, ctxt in items:
+            ctxt = str(ctxt[:-1])
+            mean = self.get_mean_estimate(actions[action])
+            self.mean_estimate[ctxt].append(mean)
+            self.ucb_estimate[ctxt] += self.get_opt_estimate(actions[action])
+            self.unique_contexts[ctxt].append(action)
+
+        # Choose to make a desicion on the pair with the highes opt value
+        self.last_context = max(self.ucb_estimate, key=self.ucb_estimate.get)
+        # choose the action with the highest value
+        ctxt_val = self.mean_estimate[self.last_context]
+        argmax = 0 if ctxt_val[0] > ctxt_val[1] else 1
+        self.last_action = self.unique_contexts[self.last_context][argmax]
+        self.contexts_played.append(actions[self.last_action])
+        return self.last_action
+
+    
+    def get_mean_estimate(self, context):
+        mean = torch.dot(self.theta, context)
+        return mean
+
+    def get_opt_estimate(self, context):
+        upper_dev = self.explore_param * sqrt(square_matrix_norm(self.A_inv, context))
+        return upper_dev
+        
 
     def get_ucb_estimate(self, context):
         # Dont have self.theta.T here.
         mean = torch.dot(self.theta, context)
-        upper_dev = self.beta * sqrt(square_matrix_norm(self.A_inv, context))
+        upper_dev = self.explore_param * sqrt(square_matrix_norm(self.A_inv, context))
 
         return mean + upper_dev
 
---
+++ metric_bandits/scripts/linucb_mnist_similarity_active.py
@@ -0,0 +1,19 @@
+from metric_bandits.algos.linucb import LinUCB
+from metric_bandits.envs.mnist_env import MNISTSimEnv
+
+# Constants for the environment
+T = 10000
+batch_size = 10
+persistence = 1
+context = "quadratic"
+
+# set up the enviromenent and model
+algo = LinUCB(active=True)
+env = MNISTSimEnv(algo, T, batch_size, persistence, pca_dims=20, context=context)
+env.reset()
+
+print("Running contextual LinUCB on MNIST dataset")
+
+# Run the algorithm
+env.train()
+print("trained")
