4b9ee5a986ed52db93e33b7849196745ee961325
Nicolas Beltran <nb2838@columbia.edu>
2022-04-29 08:48:25 -0400

    added blobs dataset

---
+++ metric_bandits/data/blobs.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+
+N_POINTS = 1000
+
+transform = np.array([[1, 0], [0, 3]])
+X = np.random.randn(N_POINTS, 2) * 0.1
+X = np.dot(X, transform)
+Y = np.random.binomial(1, 0.5, N_POINTS)
+
+X_left = X[Y == 0]
+X_left[:, 0] = X_left[:, 0] - 1
+X_right = X[Y == 1]
+X_right[:, 0] = X_right[:, 0] + 1
+
+Y_left = Y[Y == 0]
+Y_right = Y[Y == 1]
+
+X = np.vstack((X_left, X_right))
+Y = np.hstack((Y_left, Y_right))
+
+BLOBS = (torch.tensor(X), torch.tensor(Y))
